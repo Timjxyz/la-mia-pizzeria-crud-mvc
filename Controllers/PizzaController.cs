a18ffa1d92025e567fe3ca994a37262169ef100a
Timothy Gaither <timjgaither@gmail.com>
2022-07-19 15:12:25 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+        // GET: MenuPizze
+        public IActionResult Index()
+        {
+            using(PizzaContext context = new PizzaContext())
+            {
+                List<Pizza> pizzaList = context.Pizzas.ToList();
+                return View(pizzaList);
+
+            }
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza pizzaFound = context.Pizzas.Where(pizza => pizza.PizzaId == id).FirstOrDefault();
+
+                if (pizzaFound == null)
+                {
+                    return NotFound($"La pizza con id {id} non è stato trovato");
+                }
+                else
+                {
+                    return View("Details", pizzaFound);
+                }
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", pizza);
+            }
+
+            using (PizzaContext context = new PizzaContext())
+            {
+                context.Pizzas.Add(pizza);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza pizza = context.Pizzas.Where(p => p.PizzaId == id).FirstOrDefault();
+
+                if (pizza == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(pizza);
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(pizza);
+            }
+
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza editPizza=context.Pizzas.Where(p => p.PizzaId == id).FirstOrDefault();    
+                if(editPizza == null)
+                {
+                    return NotFound();
+                }
+
+
+                editPizza.Name = pizza.Name;
+                editPizza.Img = pizza.Img;
+                editPizza.Description=pizza.Description;
+                editPizza.Price = pizza.Price;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza pizza = context.Pizzas.Where(pizza => pizza.PizzaId == id).FirstOrDefault();
+
+                if(pizza == null)
+                {
+                    return NotFound();
+                }
+
+                context.Pizzas.Remove(pizza);   
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+        }
+
+
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,25 @@
+﻿using la_mia_pizzeria_static.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("pizza")]
+public class Pizza
+    {
+        [Key]
+        public int PizzaId { get; set; }
+        [StringLength(20, ErrorMessage ="Il numero massimo di caratteri inseribili è 20 caratteri")]
+        [Required(ErrorMessage ="Il campo è obbligatorio")]
+        public string Name { get; set; }
+        [ValidatorPizzaForm]
+        [StringLength(100, ErrorMessage = "Il numero massimo di caratteri inseribili è 100 caratteri")]
+        public string Description { get; set; }
+       
+        public string Img { get; set; }
+        [Range(1, 100, ErrorMessage = "Il prezzo selezionato non è valido, min 1/ max 100")]
+        public double Price { get; set; }
+    public Pizza()
+    {
+
+    }
+}
+
